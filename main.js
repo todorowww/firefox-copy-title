405c388fdb0ef593dcb4528104e5c00478112d43
Slobodan Todorov <slobodan@todorowww.net>
2025-01-31 15:08:36 +0100

    Commit number 1.

---
+++ main.js
@@ -0,0 +1,29 @@
+browser.runtime.onInstalled.addListener(() => {
+    browser.contextMenus.create({
+        id: "copy-page-title",
+        title: "Copy Page Title",
+        contexts: ["tab"],
+        icons: {
+            "16": "icons/title-16.png"
+        }
+    });
+
+    browser.contextMenus.create({
+        id: "copy-page-url",
+        title: "Copy Page URL",
+        contexts: ["tab"],
+        icons: {
+            "16": "icons/url-16.png"
+        }
+    });
+});
+
+browser.contextMenus.onClicked.addListener((info, tab) => {
+    if (info.menuItemId === "copy-page-title") {
+        browser.tabs.executeScript(tab.id, { code: "navigator.clipboard.writeText(document.title);" })
+            .catch(err => console.error("Failed to copy title: ", err));
+    } else if (info.menuItemId === "copy-tab-url") {
+        browser.tabs.executeScript(tab.id, { code: "navigator.clipboard.writeText(window.location.href);" })
+            .catch(err => console.error("Failed to copy URL: ", err));
+    }
+});
